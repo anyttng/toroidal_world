2d8edfe789a176bd2c1664bde458454a4291b355
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-17 17:34:32 +0300

    fix(world): flee a hurt squid away from its attacker across the seam

---
+++ common/src/main/java/com/toroidalworld/mixin/SquidFleeAimMixin.java
@@ -0,0 +1,24 @@
+package com.toroidalworld.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import com.toroidalworld.engine.seam.SeamAim;
+
+import net.minecraft.world.entity.animal.Squid;
+import net.minecraft.world.phys.Vec3;
+
+@Mixin(targets = "net.minecraft.world.entity.animal.Squid$SquidFleeGoal")
+public class SquidFleeAimMixin {
+    @Shadow(aliases = {"this$0", "field_6914"})
+    @Final
+    private Squid squid;
+
+    @ModifyVariable(method = "tick", at = @At(value = "STORE", ordinal = 0))
+    private Vec3 toroidal$fleeDeltaThroughSeam(Vec3 delta) {
+        return SeamAim.foldDelta(this.squid, delta);
+    }
+}
