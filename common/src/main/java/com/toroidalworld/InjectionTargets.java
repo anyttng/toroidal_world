0fd0e4ba31a371c75dfd7d0731d4ef9030aa560c
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-18 15:58:01 +0300

    fix(world): fold the player delta before the sign's side pick at the seam

--- common/src/main/java/com/toroidalworld/InjectionTargets.java
+++ common/src/main/java/com/toroidalworld/InjectionTargets.java
@@ -47,6 +47,8 @@ public final class InjectionTargets {
 
     public static final String LIVING_ENTITY_GET_Z = "Lnet/minecraft/world/entity/LivingEntity;getZ()D";
 
+    public static final String MTH_ATAN2 = "Lnet/minecraft/util/Mth;atan2(DD)D";
+
     public static final String PATHFINDER_MOB_GET_HOME_POSITION =
             "Lnet/minecraft/world/entity/PathfinderMob;getHomePosition()Lnet/minecraft/core/BlockPos;";
 
