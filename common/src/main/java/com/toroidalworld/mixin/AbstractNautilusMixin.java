3ec914f3c231474a7e65fc16c0dc784bf5cc8e6c
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-18 12:41:37 +0300

    fix(world): fold the home restriction distance of a happy ghast and a nautilus across the seam

--- common/src/main/java/com/toroidalworld/mixin/AbstractNautilusMixin.java
+++ common/src/main/java/com/toroidalworld/mixin/AbstractNautilusMixin.java
@@ -5,30 +5,20 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import com.toroidalworld.InjectionTargets;
 import com.toroidalworld.engine.seam.SeamRange;
-import com.toroidalworld.engine.seam.SeamSteering;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
-import net.minecraft.world.entity.animal.happyghast.HappyGhast;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.entity.animal.nautilus.AbstractNautilus;
 
-@Mixin(HappyGhast.class)
-public class HappyGhastMixin {
-    @ModifyExpressionValue(
-            method = "scanPlayerAboveGhast",
-            at = @At(value = "INVOKE", target = InjectionTargets.ENTITY_POSITION))
-    private Vec3 toroidal$riderPositionThroughSeam(Vec3 position) {
-        return SeamSteering.nearestCopy((HappyGhast) (Object) this, position);
-    }
-
+@Mixin(AbstractNautilus.class)
+public class AbstractNautilusMixin {
     @WrapOperation(
             method = "checkRestriction",
             at = @At(value = "INVOKE", target = InjectionTargets.BLOCK_POS_CLOSER_THAN))
     private boolean toroidal$homeRestrictionThroughSeam(BlockPos home, Vec3i pos, double distance,
             Operation<Boolean> original) {
-        return SeamRange.closerThan((HappyGhast) (Object) this, home, pos, distance);
+        return SeamRange.closerThan((AbstractNautilus) (Object) this, home, pos, distance);
     }
 }
