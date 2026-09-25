957c961bde96f0cff8347dc7919ac3cda8572908
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-25 14:24:16 +0300

    fix(world): fold BetterEnd's End tunnels across the seam

--- common/src/main/java/com/toroidalworld/compat/betterend/BetterEndInjectionTargets.java
+++ common/src/main/java/com/toroidalworld/compat/betterend/BetterEndInjectionTargets.java
@@ -3,6 +3,8 @@ package com.toroidalworld.compat.betterend;
 public final class BetterEndInjectionTargets {
     public static final String NOISE_EVAL_2D = "Lorg/betterx/betterend/noise/OpenSimplexNoise;eval(DD)D";
 
+    public static final String NOISE_EVAL_3D = "Lorg/betterx/betterend/noise/OpenSimplexNoise;eval(DDD)D";
+
     private BetterEndInjectionTargets() {
     }
 }
