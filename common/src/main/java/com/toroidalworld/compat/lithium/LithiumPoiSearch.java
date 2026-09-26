e37a5d2d0af34f8f26f219840f8993a36a9f9c7b
Vitalik Yarina <vitalikyarina.1999@gmail.com>
2026-09-26 19:47:14 +0300

    fix(world): gate the AcquirePoi retry mixin on Lithium's tasks option

--- common/src/main/java/com/toroidalworld/compat/lithium/LithiumPoiSearch.java
+++ common/src/main/java/com/toroidalworld/compat/lithium/LithiumPoiSearch.java
@@ -6,9 +6,19 @@ public final class LithiumPoiSearch {
             "(Ljava/util/function/Predicate;Ljava/util/function/Predicate;Lnet/minecraft/core/BlockPos;I"
                     + "Lnet/minecraft/world/entity/ai/village/poi/PoiManager$Occupancy;J)Ljava/util/Collection;")
             .read(LithiumPoiSearch.class.getClassLoader());
+    private static final boolean RETRY_APPLIES = new LithiumOptionGate("[lithium-compat] gate ai_poi_tasks",
+            LithiumInjectionTargets.TASKS_ACQUIRE_POI_MIXIN, LithiumInjectionTargets.RETRY_MARKER_LAMBDA,
+            "(Lnet/minecraft/core/BlockPos;JLnet/minecraft/world/entity/ai/village/poi/PoiManager;"
+                    + "Ljava/util/function/Predicate;Ljava/util/function/Predicate;J"
+                    + "Lnet/minecraft/world/entity/ai/behavior/AcquirePoi$JitteredLinearRetry;)V")
+            .read(LithiumPoiSearch.class.getClassLoader());
 
     public static boolean applies() {
         return APPLIES;
+    }
+
+    public static boolean retryApplies() {
+        return RETRY_APPLIES;
     }
 
     private LithiumPoiSearch() {
